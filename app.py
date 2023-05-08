b7dd1129168c5d3a4c1899337a0088106f611af1
Allan Juan <allanjuansil@gmail.com>
2023-05-07 22:38:23 -0300

    Create endpoint to fetch real descriptions

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request 
+import random
 
+def load_real_descriptions():
+  with open('real-descriptions.txt', "r", encoding="utf-8") as f:
+    return [line.strip() for line in f.readlines()]
+
+real_descriptions = load_real_descriptions()
 app = Flask(__name__)
 
-@app.route('/descriptions')
-def index():
+@app.route('/descriptions/real')
+def get_real_descriptions():
   count = int(request.args.get('count'))
+  return random.sample(real_descriptions, count)
 
-  return ['Random description' for i in range(count)]
+
